b47f2241cb30c1e020c62b6a6dd556d476774d45
Ashish Adhikari <amazingashish061@gmail.com>
2021-12-04 23:19:23 +0545

    comit time

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask import request
 from flask import render_template
 import time
+import asyncio
+
 
 app = Flask(__name__)
 
-def long_load(typeback):
-    time.sleep(32) #just simulating the waiting period
+async def long_load(typeback):
+    time.sleep(5) #just simulating the waiting period
     return "You typed: %s" % typeback
 
 @app.route('/')
@@ -18,11 +20,11 @@ def loading():
     return render_template ("loading.html")
 
 @app.route('/task')
-def task():
+async def task():
     #query = request.form['anything']
     query = "oh"
-    outcome = long_load(query)
-    return render_template("done.html", display=outcome)
+    final_file =  await long_load(query)
+    return render_template("done.html", display=final_file)
 
 
 if __name__ == '__main__':
